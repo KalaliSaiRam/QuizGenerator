22b7d4afd46e02f247919e5b75a8e318f680dd6a
Kalali Sai Ram Goud <ram.goud2524@gmail.com>
2026-04-22 21:39:40 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,8 @@
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from routes.quiz_routes import router
+
+app = FastAPI()
 
 origins = [
     "https://quiz-generator-frontend-kappa.vercel.app"
@@ -11,3 +15,13 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+@app.get("/")
+def home():
+    return {"message": "API running"}
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
+app.include_router(router)
